c47976ab6ab5ea59aa6547543abc4b547008ed63
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-03-07 01:29:18 +0200

    list expenses and refactor supabase code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
