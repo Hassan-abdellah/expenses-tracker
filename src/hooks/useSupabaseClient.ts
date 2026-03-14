88083967ab15aba28089bab7db6144e284b124f3
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-03-15 00:57:56 +0200

    first integration of clerk

--- src/hooks/useSupabaseClient.ts
+++ src/hooks/useSupabaseClient.ts
@@ -1,9 +1,16 @@
+import { useSession } from "@clerk/react";
 import { createClient } from "@supabase/supabase-js";
 
 export const useSupabaseClient = () => {
+  const { session } = useSession();
   const supabaseClient = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
+    {
+      async accessToken() {
+        return session?.getToken() ?? null;
+      },
+    },
   );
 
   return supabaseClient;
