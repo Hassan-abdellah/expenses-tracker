be2daabdbb3c24e3345db6cdfe2ee39312cc36d3
Hassan Abdellah <hassanabdellah25@gmail.com>
2026-04-14 21:46:04 +0200

    fix clerk user profile issue on mobile / sidebar issue on mobile

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -182,7 +182,12 @@ function Sidebar({
 
   if (isMobile) {
     return (
-      <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
+      <Sheet
+        open={openMobile}
+        onOpenChange={setOpenMobile}
+        {...props}
+        modal={false}
+      >
         <SheetContent
           data-sidebar="sidebar"
           data-slot="sidebar"
